14015865dd7fa7d33b378129836d47c237c560d5
Ned-el-ch <nedelchev.niki@gmail.com>
2019-11-28 16:55:12 +0000

    Done.

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const returnFirstTwoDrivers = (arr) => {
 
 	return arr.slice(0, 2)
 
-}
+};
 
 const returnLastTwoDrivers = (arr) => {
 
 	return arr.slice(-2)
 
-}
-
-const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+};
 
 const createFareMultiplier = (value) => {
 
@@ -21,22 +19,24 @@ const createFareMultiplier = (value) => {
 
 	}
 
-}
+};
 
 const fareDoubler = (input) => {
 
 	return input * 2;
 
-}
+};
 
 const fareTripler = (input) => {
 
 	return input * 3;
 
-}
+};
 
 const selectDifferentDrivers = (arrayOfDrivers, func) => {
 
 	return func(arrayOfDrivers);
 
-}
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
