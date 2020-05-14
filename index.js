8afb9c46470850bee2601cfc14d5f1a776646291
bogomilam <bogomilmihaylov@live.com>
2020-05-14 23:04:14 +0100

    selecta method

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ const returnFirstTwoDrivers = function(drivers) {
 const returnLastTwoDrivers = function(drivers) {
         return drivers.slice(Math.max(drivers.length - 2, 0))
         }
+
+const selectingDrivers = [  returnFirstTwoDrivers, returnLastTwoDrivers]
