465778cc54979ceb1204d7328a5a21e1789e7682
bogomilam <bogomilmihaylov@live.com>
2020-05-14 22:40:35 +0100

    first two fn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2)
+    }
+
+const returnLastTwoDrivers = function(drivers) {
+        return drivers.slice(Math.max(drivers.length - 2, 0))
+        }
