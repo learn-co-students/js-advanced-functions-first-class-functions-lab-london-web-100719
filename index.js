495c61d2a019fabbbd2620de13199d7255e3e1e1
bogomilam <bogomilmihaylov@live.com>
2020-05-15 11:46:28 +0100

    Done.

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ const returnFirstTwoDrivers = function(drivers) {
     }
 
 const returnLastTwoDrivers = function(drivers) {
-        return drivers.slice(Math.max(drivers.length - 2, 0))
+        return drivers.slice(- 2)
         }
 
 const selectingDrivers = [  returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(num) {
+    return function(fare) {
+        return fare * num 
+    }
+    }
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function(drivers, whichDrivers) {
+        return whichDrivers(drivers)
+}
